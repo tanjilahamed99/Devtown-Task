9a65103032b074051bda92feb3b1706db88c7954
Tanjil Ahamed <tanjil113355@gmail.com>
2023-12-21 20:18:40 +0600

    website host on firebase

--- src/Hooks/useAxios/UseAxios.jsx
+++ src/Hooks/useAxios/UseAxios.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000'
+    // baseURL: 'http://localhost:5000'
+    baseURL: 'https://mobile-store-server-three.vercel.app'
 })
 
 
